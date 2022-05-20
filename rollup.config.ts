05aa81e2a0105ee2398968c38d1b4f6a08ba2b38
Matthew Maxwell <maxwellmattryan@gmail.com>
2022-05-20 17:27:20 -0500

    chore: improve dev environment to support running as script and bundling

--- rollup.config.ts
+++ rollup.config.ts
@@ -6,5 +6,5 @@ export default {
         { file: 'dist/index.mjs', format: 'esm' },
         { file: 'dist/index.js', format: 'cjs' },
     ],
-    plugins: [typescript()],
+    plugins: [typescript({ tsconfig: './tsconfig.build.json' })],
 }
